ec08f30158c051d5ab5ad3fb2213d0e76627339f
Dylan.Luo <impala8012@gmail.com>
2023-05-09 13:10:09 +0800

    fibonaci

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,46 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// solution 1: linear runtime
+// function fib(n) {
+//    const result = [0,1]
+//    for(let i = 2; i <= n; i++) {
+//     const a = result[i-1]
+//     const b = result[i-2]
 
+//     result.push(a+b)
+//    }
+
+//    return result[n]
+// }
+
+
+// solution 2 recursive: exponential runtime
+function slowFib(n) {
+  if(n < 2){
+    return n
+  }
+
+  return fib(n -1) + fib(n-2)
+  // we can find out the identical function will be called many times
+  // if we want to improve it. => memorization: Store the arguments of each function call along with the result.
+  // if the function is called again with the same arguments, return the precomputed result, rather than running the function again
+}
+
+
+// solution 2 recursive: exponential runtime
+function memorization(fn) {
+  const cache = {}
+  return function(...args) {
+    if(cache[args]){
+      return cache[args]
+    }
+    const result = fn.apply(this,args)
+    cache[args] = result
+  
+    return result
+  }
+}
+
+const fib = memorization(slowFib)
 module.exports = fib;
