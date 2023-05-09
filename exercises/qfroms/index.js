86b82828c156c8818914ad8145ceccf887908ba6
Dylan.Luo <impala8012@gmail.com>
2023-05-09 14:52:57 +0800

    qfroms

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,45 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+  constructor(){
+    this.first = new Stack()
+    this.second = new Stack()
+  }
+  add(record){
+    this.first.push(record)
+  }
+  remove(){
+    while(this.first.peek()){
+      this.second.push(this.first.pop())
+    }
+
+    const record = this.second.pop()
+    
+    while(this.second.peek()){
+      // put everything back to the stack A
+      this.first.push(this.second.pop())
+    }
+
+    return record
+  }
+
+  peek(){
+    while(this.first.peek()){
+      // take the record and put into the second stack
+      this.second.push(this.first.pop())
+    }
+
+    // get reference to the top record inside of second
+    const record = this.second.peek()
+
+    while(this.second.peek()){
+      this.first.push(this.second.pop())
+    }
+
+    // return the reference to the second record
+    return record
+  }
+}
 
 module.exports = Queue;
